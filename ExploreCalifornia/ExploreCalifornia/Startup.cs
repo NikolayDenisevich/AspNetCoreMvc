01bb81f3ff5a6dbc86d191bac2c68aa986d88bc1
Nikolay Denisevich <n_denisevich@inbox.ru>
2024-02-07 14:15:02 +0300

    Created web app with static files

---
+++ ExploreCalifornia/ExploreCalifornia/Startup.cs
@@ -0,0 +1,31 @@
+﻿namespace ExploreCalifornia
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseFileServer();
+
+            /*app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });*/
+        }
+    }
+}
